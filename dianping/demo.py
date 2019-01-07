00eb27d9cae9a1d085b26a34e120484fc7607448
longxiaofei <523461639@qq.com>
2019-01-07 13:28:03 +0800

    20190107

---
+++ dianping/demo.py
@@ -0,0 +1,13 @@
+from dianping import DianpingComment
+
+COOKIES = ''
+
+class Customer(DianpingComment):
+    
+    def _data_pipeline(self, data):
+        print(data)
+
+
+if __name__ == "__main__":
+    dianping = Customer('3262456', cookies=COOKIES)
+
